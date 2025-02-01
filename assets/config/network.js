adf7b914b2dc73306b1fb30ca840a9bf7f5d5c6d
FeiFei <Virus_Cui@outlook.com>
2025-02-01 23:43:28 +0800

    Changes

--- assets/config/network.js
+++ assets/config/network.js
@@ -1,5 +1,5 @@
 const env = 'prod'
-const SERVER_IP = '/api'
-const DEVELOP_IP = '127.0.0.1:8080'
+const SERVER_IP = 'https://pan.mrcsh.cn'
+const DEVELOP_IP = '110.42.67.157:8891'
 export const baseURL = env === 'dev'?`http://${DEVELOP_IP}`:SERVER_IP
 export const wsURL = env === 'dev'?`ws://${DEVELOP_IP}`:SERVER_IP;
